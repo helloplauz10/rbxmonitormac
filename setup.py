d9c37e7be3c14923d69d7280f09426561c7bcf6f
cuteuwu <105611677+helloplauz10@users.noreply.github.com>
2024-01-25 19:39:39 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="rbxmonitor",
+    version="0.0.1",
+    description="A simple script that monitors roblox asynchronously without modifying and reading the game's client. This only works in MacOS",
+    author="cuteuwu",
+    packages=["rbxmonitor"],
+    url="https://github.com/helloplauz10/rbxmonitormac/",
+    license="MIT License",
+    install_requires=["psutil"],
+    classifiers=['Operating System :: MacOS :: MacOS X', 'Programming Language :: Python']
+)

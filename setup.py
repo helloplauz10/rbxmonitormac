1918acc5555e5d0b20ce3ce3d1653953cc80e88d
cuteuwu <105611677+helloplauz10@users.noreply.github.com>
2024-01-25 19:52:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="rbxmonitor",
+    name="rbxmonitormac",
     version="0.0.1",
     description="A simple object oriented script that monitors roblox asynchronously without modifying and reading the game's client. This only works in MacOS",
     author="cuteuwu",
